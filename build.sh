57575a5b819091cf478cd4d8081bdb76534d6799
Edwin Biemond <biemond@gmail.com>
2014-07-17 23:58:17 +0200

    update for CI tools

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ export PATH=~/0.6.0_darwin_amd64:$PATH
 rm build/OEL6_5.box || true
 packer build oracle-linux-6.5-x86_64.json
 vagrant box remove OEL6_5 || true
-vagrant box add vagrant_machine build/OEL6_5.box
+vagrant box add OEL6_5 build/OEL6_5.box
